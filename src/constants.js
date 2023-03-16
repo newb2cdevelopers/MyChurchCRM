fa8aed5b797416458f7849ec9100953a99ac673b
Danny Gomez <newb2cdevelopers@gmail.com>
2023-03-15 22:13:20 -0500

    change api url

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,2 @@
 //export const B2C_BASE_URL = "http://localhost:4000";
-export const B2C_BASE_URL = "https://b2cback.herokuapp.com";
-
-
+export const B2C_BASE_URL = "https://mychurchcrm.azurewebsites.net/api";
