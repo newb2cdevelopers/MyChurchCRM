685c04eea47894b5aabf924f4c6f111cb52fec37
Danny Gomez <newb2cdevelopers@gmail.com>
2023-03-17 17:33:20 -0500

    evento no data and cards styles

--- src/components/common/backdroploader/index.jsx
+++ src/components/common/backdroploader/index.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
 
-const BackdropLoader = ({show, message}) => {
+const BackdropLoader = ({ show, message }) => {
   return (
     <Backdrop
-    sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-    open={show}
-  >
-   <span>{message}</span>
-   <CircularProgress color="inherit" />
-  </Backdrop>
+      sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      open={show}
+    >
+      <div style={{
+        display: 'flex', flexDirection: 'column-reverse',
+        alignItems: 'center'
+      }}>
+        <span style={{ marginTop: '30px', fontSize:'20px' }}>{message}</span>
+        <CircularProgress color="inherit" sx={{ marginLeft: '15px' }} />
+      </div>
+    </Backdrop >
   )
 }
 
--- src/components/promotionalEvents/index.jsx
+++ src/components/promotionalEvents/index.jsx
@@ -5,15 +5,17 @@ import { CircularProgress } from "@mui/material";
 import { useSelector } from "react-redux";
 import { B2C_BASE_URL } from "../../constants";
 import { genericGetService } from "../../api/externalServices";
-import eventImage from "../../images/event.jpg";
+import eventImage from "../../images/data_alert.svg";
 import arrowLeft from "../../images/arrow left.svg";
 import arrowRight from "../../images/arrow right.svg";
+import BackdropLoader from '../common/backdroploader';
 
 export default function PromotionalEvents() {
   const data = useSelector((state) => state.user.selectedChurchId);
   const [events, setEvents] = useState([])
   const [currentEvents, setCurrentEvents] = useState([])
   const [nextEvents, setNextEvents] = useState([])
+  const [loading, setLoading] = useState(false);
 
   const BASE_URL = B2C_BASE_URL;
 
@@ -23,15 +25,20 @@ export default function PromotionalEvents() {
 
   useEffect(() => {
     if (data) {
+      
+      setLoading(true);
       getEvents().then(data => {
-
+        
         if (data[0]) {
           setEvents(data[0].filter(e => { return e.status === "Pendiente" }))
 
           setCurrentEvents(data[0].filter(event => { return event.isBookingAvailable && event.status === "Pendiente" }));
           setNextEvents(data[0].filter(event => { return !event.isBookingAvailable && event.status === "Pendiente" }));
+
+          setLoading(false)
         } else {
           alert("Se ha presentado un error");
+          setLoading(false)
         }
       });
     }
@@ -39,7 +46,7 @@ export default function PromotionalEvents() {
 
   return (
     <>
-      {events.length > 4 ?
+      {currentEvents.length > 4 ?
         <div className={styles.arrowContainer}>
           <img src={arrowLeft} alt="" className={styles.arrowLeft}
             onClick={() => document.getElementById('containerEvents').scrollBy(-300, 0)} />
@@ -48,39 +55,52 @@ export default function PromotionalEvents() {
         </div>
         : <></>
       }
-      {events.length > 0 ?
-        <div className={styles.containerEvents}>
-          <p className={styles.titleContainerEvents}>Eventos</p>
-          <div className={styles.currentEvents} id='containerEvents'>
-            {
-              currentEvents.length > 0 ?
+      {loading ? <BackdropLoader show={loading} message="Consultando los eventos" /> :
+        currentEvents.length > 0 || nextEvents.length > 0 ?
+          <div className={styles.containerEvents}>
+            <p className={styles.titleContainerEvents}>Eventos disponibles para reserva</p>
+            <div className={styles.currentEvents} id='containerEvents'>
+              {
+                currentEvents.length > 0 ?
 
-                currentEvents.map(e => {
-                  return <PromotionalCard eventId={e._id} title={e.name} bookingAvailable={true} />
-                }) :
-                <div>
-                  <img alt="" src={eventImage} className={styles.cardImage}></img>
-                  <span className={styles.noEvents}>No hay eventos recientes</span>
-                </div>
+                  currentEvents.map(e => {
+                    return <PromotionalCard eventId={e._id} title={e.name} bookingAvailable={true} />
+                  }) :
+                  <div className={styles.cardEvents}>
+                    <img alt="" src={eventImage} className={styles.cardImage}></img>
+                    <span className={styles.noEvents}>No hay eventos recientes</span>
+                  </div>
+              }
+
+            </div>
+            <p className={styles.titleContainerEvents}>Próximos Eventos</p>
+            {nextEvents.length > 4 ?
+              <div className={styles.arrowContainer}>
+                <img src={arrowLeft} alt="" className={styles.arrowLeftNextEvents}
+                  onClick={() => document.getElementById('containerNextEvents').scrollBy(-300, 0)} />
+                <img src={arrowRight} alt="" className={styles.arrowRightNextEvents}
+                  onClick={() => document.getElementById('containerNextEvents').scrollBy(300, 0)} />
+              </div>
+              : <></>
             }
+            <div className={styles.nextEvents} id='containerNextEvents'>
+              {
+                nextEvents.length > 0 ?
 
+                  nextEvents.map(e => {
+                    return <PromotionalCard eventId={e._id} title={e.name} bookingAvailable={false} />
+                  }) :
+                  <div className={styles.cardEvents}>
+                    <img alt="" src={eventImage} className={styles.cardImage}></img>
+                    <span className={styles.noEvents}>No hay eventos recientes</span>
+                  </div>
+              }
+            </div>
           </div>
-          <p className={styles.titleContainerEvents}>Próximos Eventos</p>
-          <div className={styles.nextEvents}>
-            {
-              nextEvents.length > 0 ?
-
-                nextEvents.map(e => {
-                  return <PromotionalCard eventId={e._id} title={e.name} bookingAvailable={false} />
-                }) :
-                <div>
-                  <img alt="" src={eventImage} className={styles.cardImage}></img>
-                  <span className={styles.noEvents}>No hay eventos recientes</span>
-                </div>
-            }
-          </div>
+          : <div className={styles.cardEvents}>
+          <img alt="" src={eventImage} className={styles.cardImage}></img>
+          <span className={styles.noEvents}>No hay eventos</span>
         </div>
-        : <>Cargando Eventos...</>
       }
     </>
   );
