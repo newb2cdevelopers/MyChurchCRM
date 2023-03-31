2971c2cbe0adc8c7101b57155be275828c7f112c
Danny Gomez <newb2cdevelopers@gmail.com>
2023-03-30 23:04:40 -0500

    link

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,4 +1,4 @@
-import  React, {useState} from 'react';
+import React, { useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -6,7 +6,7 @@ import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import LinkMu from '@mui/material/Link';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Alert from '@mui/material/Alert';
@@ -16,7 +16,7 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { genericPostService } from "../../api/externalServices";
 import BackdropLoader from "../common/backdroploader";
-import { useDispatch  } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { login, setSelectedChurch } from '../../features/user/userSlice'
 import { useNavigate } from "react-router-dom";
 import { B2C_BASE_URL } from '../../constants';
@@ -35,7 +35,7 @@ function Copyright(props) {
   );
 }
 
-const requiredFields =  [
+const requiredFields = [
   "email",
   "password"
 ];
@@ -73,46 +73,46 @@ function Login() {
 
     for (const [k, v] of Object.entries(loginInfo)) {
 
-      if(v === ""){
+      if (v === "") {
         missingFields.push(k);
       }
     }
 
-    if (missingFields.length > 0){
+    if (missingFields.length > 0) {
       setMissingRequiredFields(missingFields);
       return;
-    } 
+    }
 
     setLoading(true);
     const results = await genericPostService(`${BASE_URL}/login`, loginInfo);
     setLoading(false);
 
-    if (results[0] && results[0].access_token){
+    if (results[0] && results[0].access_token) {
       dispatch(
         login({
-          userEmail:  loginInfo.user,
+          userEmail: loginInfo.user,
           token: results[0].access_token,
           roles: results[0].roles
         })
       );
-      
+
       dispatch(
         setSelectedChurch({
-          selectedChurchId:  results[0].churchId,
+          selectedChurchId: results[0].churchId,
         })
       );
-      
+
       setErrorMessage("");
       return navigate("/dashboard");
 
     }
-    
-    if(results[0] && !results[0].access_token){
+
+    if (results[0] && !results[0].access_token) {
       setErrorMessage("Por favor verifique sus credenciales.")
       return;
     }
 
-    if(!results[0]){
+    if (!results[0]) {
       setErrorMessage("Se ha presentado un error, por favor contacte al administrador")
       return;
     }
@@ -122,13 +122,13 @@ function Login() {
   const handleFormOnchange = (e) => {
     const { name, value } = e.target
 
-    if(errorMessage.length > 0){
+    if (errorMessage.length > 0) {
       setErrorMessage("")
     }
-    if(value){
+    if (value) {
       setMissingRequiredFields([])
     }
-    setLoginInfo({...loginInfo, [name] : value} );
+    setLoginInfo({ ...loginInfo, [name]: value });
   }
 
   return (
@@ -159,8 +159,8 @@ function Login() {
               fullWidth
               label="Correo eléctrónico"
               name="user"
-              helperText= {missingRequiredFields.indexOf("user") !== -1 ? "El campo es requerido"  : ""}
-              error = {missingRequiredFields.indexOf("user") !== -1 ? true: false}
+              helperText={missingRequiredFields.indexOf("user") !== -1 ? "El campo es requerido" : ""}
+              error={missingRequiredFields.indexOf("user") !== -1 ? true : false}
               autoFocus
               onChange={handleFormOnchange}
               value={loginInfo.user}
@@ -175,8 +175,8 @@ function Login() {
               type="password"
               onChange={handleFormOnchange}
               value={loginInfo.pass}
-              helperText= {missingRequiredFields.indexOf("pass") !== -1 ? "El campo es requerido"  : ""}
-              error = {missingRequiredFields.indexOf("pass") !== -1 ? true: false}
+              helperText={missingRequiredFields.indexOf("pass") !== -1 ? "El campo es requerido" : ""}
+              error={missingRequiredFields.indexOf("pass") !== -1 ? true : false}
             />
             <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
@@ -192,12 +192,13 @@ function Login() {
             </Button>
             <Grid container>
               <Grid item xs>
-                <LinkMu href="/recoveryPassword" variant="body2">
+                <Link to="/recoveryPassword" className='text-link'><LinkMu variant="body2">
                   ¿Olvidó la contraseña?
                 </LinkMu>
+                </Link>
               </Grid>
               <Grid item>
-                <Link to="/register" className='text-link'><LinkMu  variant="body2" component={"span"}>Registrarse</LinkMu> </Link> 
+                <Link to="/register" className='text-link'><LinkMu variant="body2" component={"span"}>Registrarse</LinkMu> </Link>
               </Grid>
             </Grid>
             <div>
