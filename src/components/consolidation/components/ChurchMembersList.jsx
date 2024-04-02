8325a2423336a191f95dd164f1c7bd3113aa0e16
Danny Gomez <newb2cdevelopers@gmail.com>
2024-04-02 16:16:34 -0500

    workfront and comments in info general

--- src/components/consolidation/components/ChurchMembersList.jsx
+++ src/components/consolidation/components/ChurchMembersList.jsx
@@ -81,7 +81,8 @@ export default function ChurchMembersList() {
 
         if (document !== '' && document.length >= 3) {
             let _filteredMembers = members.filter(member => {
-                return member.documentNumber.toString().indexOf(document) >= 0
+                return member.documentNumber.toString().indexOf(document) >= 0 ||
+                 member.fullName.toString().indexOf(document) >= 0
             });
 
             if (_filteredMembers && _filteredMembers.length > 0) {
@@ -97,8 +98,8 @@ export default function ChurchMembersList() {
             <div className={styles.tableMembersContainer}>
                 <div className={styles.searchContainer}>
                     <div style={{ display: 'flex', flexDirection: 'column' }}>
-                        <span>Número de documento: </span>
-                        <input className={styles.inputField} type="text" id="idTxtMember" onChange={(e) => { searchMember(e.target.value) }} placeholder={"Ingrese el # documento"} />
+                        <span>Buscar por número de documento o nombre: </span>
+                        <input className={styles.inputField} type="text" id="idTxtMember" onChange={(e) => { searchMember(e.target.value) }} placeholder={"Ingrese el # documento o nombre"} />
                         <span>Cantidad de registros: {membersList.length}</span>
                     </div>
                     <div className={styles.createNewMemberContainer}>
@@ -117,6 +118,7 @@ export default function ChurchMembersList() {
                                 <th>Acciones</th>
                                 <th><p>Identificación</p></th>
                                 <th><p>Nombre completo</p></th>
+                                <th><p>Frente o área de trabajo</p></th>
                                 <th><p>Dirección</p></th>
                                 <th><p>Teléfono</p></th>
                                 <th><p>Celular</p></th>
@@ -142,7 +144,8 @@ export default function ChurchMembersList() {
                                             </div>
                                         </td>
                                         <td>{member.documentNumber}</td>
-                                        <td>{member.fullName}</td>
+                                        <td>{member.fullName.toUpperCase()}</td>
+                                        <td>{member.workfront ? member.workfront.name : ''}</td>
                                         <td>{member.address}</td>
                                         <td>{member.landLine}</td>
                                         <td>{member.mobilePhone}</td>
