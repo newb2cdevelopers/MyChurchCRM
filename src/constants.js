6da0dd6d7e2bcbbcc70cc543d4f71fda91ec5c83
Danny Gomez <newb2cdevelopers@gmail.com>
2025-01-28 10:31:35 -0500

    test

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-//export const B2C_BASE_URL = "http://localhost:4000";
-export const B2C_BASE_URL = "https://mychurchcrm.azurewebsites.net/api";
+export const B2C_BASE_URL = "http://localhost:4000";
+//export const B2C_BASE_URL = "https://mychurchcrm.azurewebsites.net/api";
